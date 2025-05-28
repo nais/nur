7aa98a802a202b0adff2a53e7b62d351a3a46980
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-05-28 12:04:31 +0200

    restrict systems to systems we support

--- flake.nix
+++ flake.nix
@@ -1,14 +1,21 @@
 {
-  description = "My personal NUR repository";
+  description = "Nais NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    legacyPackages = forAllSystems (system:
+      import ./default.nix {
+        pkgs = import nixpkgs {inherit system;};
       });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
-    };
+    packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+  };
 }
