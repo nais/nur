5bdda18fbd51788c5371f497897e4fb06fe51b1a
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-02 12:35:53 +0200

    build: use nixpkgs packagesFromDirectoryRecursive
    Co-authored-by: Christer Edvartsen <christer.edvartsen@nav.no>

--- default.nix
+++ default.nix
@@ -1,18 +1,11 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}}: {
+{pkgs, ...}:
+{
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib {inherit pkgs;}; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
-
-  nais-cli = pkgs.callPackage ./pkgs/nais-cli {};
-  narc-cli = pkgs.callPackage ./pkgs/narc-cli {};
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+}
+// pkgs.lib.filesystem.packagesFromDirectoryRecursive {
+  inherit (pkgs) callPackage;
+  directory = ./pkgs;
 }
