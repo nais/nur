ca8d4f0e3473c34cafce91a88acd1d6b554dd932
NAIS team app <devnull@nais.io>
2025-09-30 09:52:40 +0000

    nais-cli v3.2.7

--- pkgs/nais-cli.nix
+++ pkgs/nais-cli.nix
@@ -23,7 +23,7 @@
 in
   stdenvNoCC.mkDerivation {
     pname = "nais-cli";
-    version = "v3.2.6";
+    version = "v3.2.7";
     src = fetchurl {
       url = urlMap.${system};
       sha256 = shaMap.${system};
